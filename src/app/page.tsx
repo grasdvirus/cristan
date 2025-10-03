abeeea76dc1ff0d3c7f350292e495a808a4f0e65
Venom Grasd <grasdvenom@gmail.com>
2025-10-03 14:55:47 +0000

    supprime définitivement cette section dans l'interface site web. (_for e

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import HeroSection from '@/components/sections/hero-section';
 import ProjectsGrid from '@/components/sections/projects-grid';
-import VideoSection from '@/components/sections/video-section';
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
-      <div className="container mx-auto px-4 py-16 sm:py-24 space-y-24">
+      <div className="container mx-auto px-4 py-16 sm:py-24">
         <ProjectsGrid />
-        <VideoSection />
       </div>
     </div>
   );
