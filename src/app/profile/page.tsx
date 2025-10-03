3fe87975b0b6cc5d0ea6caaeb05c5198ef988e81
Venom Grasd <grasdvenom@gmail.com>
2025-10-03 18:41:12 +0000

    dans le cadre je préfère que ce sois la première lettre du nom de l'util

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -53,12 +53,8 @@ export default function ProfilePage() {
   );
   
   const getInitials = (name?: string | null) => {
-    if (!name) return '??';
-    const names = name.split(' ');
-    if (names.length > 1) {
-      return `${names[0][0]}${names[names.length - 1][0]}`.toUpperCase();
-    }
-    return name.substring(0, 2).toUpperCase();
+    if (!name || name.length === 0) return '?';
+    return name[0].toUpperCase();
   }
 
   return (
