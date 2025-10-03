284d5000feab2513cb7b151c293c81007bb2ebd0
Venom Grasd <grasdvenom@gmail.com>
2025-10-03 16:42:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-370357680-50dc7",
+  "appId": "1:426428459409:web:5b01fe1cb7c8a109449230",
+  "apiKey": "AIzaSyBZy1LwJb-zqlqBykT1u7bkCmz6rKo-MiE",
+  "authDomain": "studio-370357680-50dc7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "426428459409"
+};
