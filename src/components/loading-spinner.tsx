82ded7ef577fc0955eaa49ef7766a60acc3cb57a
Venom Grasd <grasdvenom@gmail.com>
2025-11-05 15:46:13 +0000

    salut je veut un changement dans je veux que les chargement utilise cett

--- src/components/loading-spinner.tsx
+++ src/components/loading-spinner.tsx
@@ -1,16 +1,14 @@
 export function LoadingSpinner() {
   return (
-    <div className="hole">
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
+    <div className="flex items-center justify-center w-full h-full">
+      <video
+        src="/charge.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="w-48 h-48 object-cover"
+      />
     </div>
   );
 }
