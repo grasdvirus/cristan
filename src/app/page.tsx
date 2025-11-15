11ef98676aa9b1be3b99d501cc773646a3b2bb00
Venom Grasd <grasdvenom@gmail.com>
2025-11-15 10:33:03 +0000

    change le nom rend le plus simple et court et retire le contour autour d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,9 +50,9 @@ export default function Home() {
                       href="https://cristian-two.vercel.app/"
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-4 py-1.5 text-lg font-bold ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-b-2 border-transparent hover:border-primary/50"
+                      className="inline-flex items-center justify-center whitespace-nowrap px-4 py-1.5 text-lg font-bold border-b-2 border-transparent hover:text-primary transition-colors"
                     >
-                      Devenir Partenaire
+                      Partenariat
                     </a>
                   </TabsList>
               </Tabs>
