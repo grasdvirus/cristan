527b55ec07095015bd14e36a116ad9364841467c
Venom Grasd <grasdvenom@gmail.com>
2025-11-20 16:55:10 +0000

    ajoute un défilement automatique sur les bandeau.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -96,12 +96,12 @@ export default {
           },
         },
         'marquee': {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
         },
         'marquee2': {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0%)' },
+          from: { transform: 'translateX(100%)' },
+          to: { transform: 'translateX(0)' },
         },
       },
       animation: {
