204dff8539bdcead72e56b2f25ae3ac266536038
Venom Grasd <grasdvenom@gmail.com>
2025-11-20 00:52:19 +0000

    Try fixing this error: `Runtime Error: Error: useState is not defined. E

--- src/app/partner/reward/page.tsx
+++ src/app/partner/reward/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useForm, Controller } from 'react-hook-form';
