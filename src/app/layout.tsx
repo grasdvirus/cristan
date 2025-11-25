71d9bba245fe745db6975d044110b1197cc3a297
Venom Grasd <grasdvenom@gmail.com>
2025-11-25 23:40:59 +0000

    annule la possibilité de scroller a la verticale  (_for element span_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased bg-background text-foreground">
+      <body>
         <AppContent>{children}</AppContent>
       </body>
     </html>
